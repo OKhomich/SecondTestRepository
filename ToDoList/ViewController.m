278a229ea56b7af598933bc6b2d38a5b70d5f774
Olga Khomich <olga.khomich13@gmail.com>
2015-08-12 18:28:43 +0300

    next add NSLog

--- ToDoList/ViewController.m
+++ ToDoList/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSString *s1 = @"Hello world";
+    NSLog(@"%@",s1);
 }
 
 - (void)didReceiveMemoryWarning {
