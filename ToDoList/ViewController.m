5a690b98fa6934db5e1f4246734230aa7f1499c5
Olga Khomich <olga.khomich13@gmail.com>
2015-08-12 19:00:08 +0300

    Revert 801c6ca..1432612
    This rolls back to commit 801c6ca75b3eb954bc38ddac37c157572f9ddf0f.

--- ToDoList/ViewController.m
+++ ToDoList/ViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSString *s1 = @"Hello world";
-    NSLog(@"%@",s1);
 }
 
 - (void)didReceiveMemoryWarning {
